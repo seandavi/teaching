99e6bc8585c858bc6badfaf2cdae29c5e4ba6c4b
Sean Davis <seandavi@gmail.com>
2022-03-30 11:23:04 -0600

    add download script

---
+++ uvi-spring-2022/labs/download-eda-lab-materials.R
@@ -0,0 +1,2 @@
+download.file('https://github.com/seandavi/teaching/raw/main/uvi-spring-2022/labs/2022-03-28-exploratory-data-analysis-lab-answers.html',destfile='2022-03-28-exploratory-data-analysis-lab-answers.html')
+download.file('https://github.com/seandavi/teaching/raw/main/uvi-spring-2022/labs/2022-03-30-exploratory-data-analysis.html',destfile = '2022-03-30-exploratory-data-analysis.html' )
